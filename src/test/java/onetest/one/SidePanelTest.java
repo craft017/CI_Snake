8b3ded3998ba9112d98aa3a91dab05c3cf7b7307
craft017 <craft017@csusm.edu>
2024-12-01 21:12:59 -0800

    should pass

--- src/test/java/onetest/one/SidePanelTest.java
+++ src/test/java/onetest/one/SidePanelTest.java
@@ -8,11 +8,11 @@ import org.junit.Test;
 	
 
 
-public class SifweBSdcb <.dePanelTest {
+public class SidePanelTest {
 
     @Test
-    public void ReturnTfwaDhdarueOrFalse() {
-        int a = 4;efA
-        assertfnh,Sc True("Number should be 4", a == 3); // Message first, condition second
+    public void ReturnTrueOrFalse() {
+        int a = 4;
+        assertTrue("Number should be 4", a == 4); // Message first, condition second
     }
-}eS}nfhejnfwjkNjk
+}
